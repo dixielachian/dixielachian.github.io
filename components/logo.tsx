41bfbaaf34045ee49fe36d348c4642b5e737e690
dixielachian <appalachiancavalier@proton.me>
2023-06-21 17:41:55 +0000

    fixed avatar img loading

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,9 +1,10 @@
 import Image from 'next/image';
+import avatar from '../public/img/avatar.jpeg';
 
 const Logo = () => {
   return (
     <>
-      <Image className='rounded-2xl' width={90} height={90} src='/img/avatar.jpeg' alt='Logo' />
+      <Image className='rounded-2xl' width={90} height={90} src={avatar} alt='Logo' />
       <h1 className='ml-6 self-center text-3xl font-semibold whitespace-nowrap text-white'>
         Dixielachian Futurism
       </h1>
